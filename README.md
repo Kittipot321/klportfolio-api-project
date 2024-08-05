8535de42577d4e4979b0e2b037d6bdc75eeb73bc
KittipotKL <kittipot.l@synergysoftware.co.th>
2024-08-05 14:39:32 +0700

    Add API with Notion Database

---
+++ README.md
@@ -0,0 +1,11 @@
+# Portfolio API
+
+## Description
+
+This is a portfolio api project with made by nodeJS
+
+## Tools
+
+- Notion Database
+- NodeJS
+- Typescript
---
+++ src/server.ts
@@ -0,0 +1,90 @@
+require("dotenv").config();
+import http from "http";
+import { Client } from "@notionhq/client";
+
+const host = "localhost";
+const port = 8000;
+
+interface Projects {
+  project_name: string;
+  subtitle: string;
+  description: string;
+  link: string;
+  year: number | null;
+}
+
+const notionDatabaseId = process.env.NOTION_DATABASE_ID;
+const notionSecret = process.env.NOTION_SECRET;
+
+if (!notionDatabaseId || !notionSecret) {
+  throw new Error("Notion credentials are missing");
+}
+
+const notion = new Client({ auth: notionSecret });
+
+const server = http.createServer(async (req, res) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+
+  switch (req.url) {
+    case "/":
+      res.writeHead(200);
+      res.end(
+        JSON.stringify({
+          message: "This is my api project with made by notion database",
+        })
+      );
+      break;
+    case "/projects":
+      res.writeHead(200);
+      const query = await notion.databases.query({
+        database_id: notionDatabaseId,
+        sorts: [
+          {
+            property: "year",
+            direction: "descending",
+          },
+        ],
+      });
+
+      const dataList: Projects[] = query.results.map((row) => {
+        const project_name =
+          row.properties.Project_name?.type === "title"
+            ? row.properties.Project_name.title[0]?.plain_text
+            : "";
+        const subtitle =
+          row.properties.Subtitle?.type === "rich_text"
+            ? row.properties.Subtitle.rich_text[0]?.plain_text
+            : "";
+        const description =
+          row.properties.Description?.type === "rich_text"
+            ? row.properties.Description.rich_text[0]?.plain_text || ""
+            : "";
+        const link =
+          row.properties.link?.type === "rich_text"
+            ? row.properties.link.rich_text[0]?.plain_text || ""
+            : "!#";
+        const year =
+          row.properties.year?.type === "number"
+            ? row.properties.year.number
+            : 0;
+
+        return {
+          project_name,
+          subtitle,
+          description,
+          link,
+          year,
+        };
+      });
+
+      return res.end(JSON.stringify({ data: dataList }));
+    default:
+      res.writeHead(404);
+      res.end(JSON.stringify({ error: "Not found" }));
+      break;
+  }
+});
+
+server.listen(port, host, () => {
+  console.log(`Server is running on http://${host}:${port}`);
+});
